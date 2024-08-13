31b935e4e22923aca9daaf9c281183b4c0ef1400
amit97s <amit1397singh@gmail.com>
2024-08-13 20:35:10 +0530

    prop-hawks

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
+    {/*  */}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
